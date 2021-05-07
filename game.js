daf1dd0df2295e62755c387c8a7eab465f51eabd
Samuel Silva <samuelsilva9@gmail.com>
2021-05-07 17:06:34 -0600

    Update game.js
    fixing refresh

--- game.js
+++ game.js
@@ -10,7 +10,7 @@ const gameBoard = document.getElementById('game-board')
 function main(currentTime){
     if(gameOver){
         if(confirm('You lost. Press ok to restart.')){
-            window.location = '/JS/Snake/index.html' // refresh page
+            window.location = '/snake' // refresh page
         }
         return
     }
